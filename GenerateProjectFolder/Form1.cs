fd7e51b22a2d086ec721c14455e7bdb7c50a9390
Gaiokane <qk632773809@vip.qq.com>
2020-08-20 23:35:59 +0800

    设计：配置文件（文件项、文件路径、生成目录文件路径）

--- GenerateProjectFolder/Form1.cs
+++ GenerateProjectFolder/Form1.cs
@@ -16,5 +16,17 @@ namespace GenerateProjectFolder
         {
             InitializeComponent();
         }
+
+        /*
+         * 整体思路：
+         * ———————配置文件———————
+         * 1.<文件项> 存 文件路径键值对中的键，分号间隔。如<add key="FileName" value="FileName_01;FileName_02;FileName_03" />
+         * 2.<文件路径> 存 文件路径，如<add key="FileName_01" value="D:/vs-program/GitHub/GenerateProjectFolder/GenerateProjectFolder.sln" />
+         * 3.<生成目录文件路径> 存 生成的目录、文件保存路径，如<add key="ProjectFolderPath" value="D:/vs-program/GitHub/" />
+         * 4.
+         * 5.
+         * 6.
+         * 7.
+         */
     }
 }
