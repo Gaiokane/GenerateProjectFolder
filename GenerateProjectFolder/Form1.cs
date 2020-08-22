17f91f669da718ddee4443c3ec61938e76fdac31
Gaiokane <qk632773809@vip.qq.com>
2020-08-22 23:28:17 +0800

    设计：主窗体

--- GenerateProjectFolder/Form1.cs
+++ GenerateProjectFolder/Form1.cs
@@ -24,9 +24,10 @@ namespace GenerateProjectFolder
          * 2.<文件路径> 存 文件路径，如<add key="FileName_01" value="D:/vs-program/GitHub/GenerateProjectFolder/GenerateProjectFolder.sln" />
          * 3.<生成目录文件路径> 存 生成的目录、文件保存路径，如<add key="ProjectFolderPath" value="D:/vs-program/GitHub/" />
          * 4.
-         * 5.
-         * 6.
-         * 7.
+         * ———————窗体设计———————
+         * 1.主窗体（项目编号[label、textbox]、项目名称[label、textbox]、确定[button]、生成至：[label、textbox、button]选择生成的项目文件夹路径、设置[button]）
+         * 2.
+         * 3.
          */
     }
 }
