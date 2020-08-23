fbc8b5701237e6374a5ba0db9509eb7e1f4fd7d7
Gaiokane <qk632773809@vip.qq.com>
2020-08-23 19:59:57 +0800

    设计：设置窗体（模板文件设置）

--- GenerateProjectFolder/Form1.cs
+++ GenerateProjectFolder/Form1.cs
@@ -25,9 +25,13 @@ namespace GenerateProjectFolder
          * 3.<生成目录文件路径> 存 生成的目录、文件保存路径，如<add key="ProjectFolderPath" value="D:/vs-program/GitHub/" />
          * 4.
          * ———————窗体设计———————
-         * 1.主窗体（项目编号[label、textbox]、项目名称[label、textbox]、确定[button]、生成至：[label、textbox、button]选择生成的项目文件夹路径、设置[button]）
-         * 2.
-         * 3.
+         * 1.主窗体（生成至：[label、textbox]、路径选择[button]、项目编号[label、textbox]、项目名称[label、textbox]、生成[button]、设置[button]）
+         * 2.主窗体—设置窗体（模板文件设置、）
+         * 3.设置窗体—模板文件设置（功能组合[groupbox]、刷新[button]、新增[button]、编辑[button]、删除[button]、列表[datagridview]：模板文件编码、模板文件名称、模板文件路径、模板文件备注）
+         * 4.模板文件设置—新增/编辑（模板文件编码[label、textbox]、模板文件名称[label、textbox]、模板文件路径[label、textbox]、路径选择[button]、模板文件备注[label、richtextbox]、保存[button]、取消[button]）
+         * 5.
+         * 6.
+         * 7.
          */
     }
 }
