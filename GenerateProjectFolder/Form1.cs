467a30e405996c3616edc323bd20cfacb8085ef6
Gaiokane <qk632773809@vip.qq.com>
2020-08-28 22:25:56 +0800

    谁：主窗体（增加项目简称）

--- GenerateProjectFolder/Form1.cs
+++ GenerateProjectFolder/Form1.cs
@@ -25,13 +25,15 @@ namespace GenerateProjectFolder
          * 3.<生成目录文件路径> 存 生成的目录、文件保存路径，如<add key="ProjectFolderPath" value="D:/vs-program/GitHub/" />
          * 4.
          * ———————窗体设计———————
-         * 1.主窗体（生成至：[label、textbox]、路径选择[button]、项目编号[label、textbox]、项目名称[label、textbox]、生成[button]、设置[button]）
+         * 1.主窗体（生成至：[label、textbox]、路径选择[button]、项目编号[label、textbox]、项目名称[label、textbox]、项目简称[label、textbox]、生成[button]、设置[button]）
          * 2.主窗体—设置窗体（模板文件设置、）
          * 3.设置窗体—模板文件设置（功能组合[groupbox]、刷新[button]、新增[button]、编辑[button]、删除[button]、列表[datagridview]：模板文件编码、模板文件名称、模板文件路径、模板文件备注）
          * 4.模板文件设置—新增/编辑（模板文件编码[label、textbox]、模板文件名称[label、textbox]、模板文件路径[label、textbox]、路径选择[button]、模板文件备注[label、richtextbox]、保存[button]、取消[button]）
          * 5.
          * 6.
          * 7.
+         * 备注：
+         * 1.项目简称：用在 xx缺陷截图、xx问题.txt、xx估算、xx项目测试进度 等
          * ———————通用方法设计———————
          * 1.配置文件默认值设置方法（对不存在的key进行新增）
          * 2.是否存在[配置文件中的key]
