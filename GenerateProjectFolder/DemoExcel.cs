a743d9832b69b4540c1ef167459bf693f681c4d2
Gaiokane <qk632773809@vip.qq.com>
2020-09-05 23:41:27 +0800

    优化：使用NOPI修改Excel指定单元格数据（优化代码）

--- GenerateProjectFolder/DemoExcel.cs
+++ GenerateProjectFolder/DemoExcel.cs
@@ -74,20 +74,9 @@ namespace GenerateProjectFolder
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //WriteToExcel(@"E:\write.xlsx");
-            //MessageBox.Show(ReadFromExcelFile(textBox1.Text, 0));
             MessageBox.Show(ReadFromExcelFileByCell(textBox1.Text, 0, 7, 2));
-            WriteToExcelByCell(textBox1.Text, 0, 7, 2, "使用NPOI写入");
+            WriteToExcelByCell(textBox1.Text, 0, 7, 2, "hhh使用NPOI写入123");
             MessageBox.Show(ReadFromExcelFileByCell(textBox1.Text, 0, 7, 2));
-
-
-            /*FileStream file = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            workbook.GetSheetAt(0).GetRow(7).GetCell(2).SetCellValue("qaq");
-            FileStream file2 = new FileStream(textBox1.Text, FileMode.Create);
-            workbook.Write(file2);
-            file2.Close();
-            MessageBox.Show(ReadFromExcelFileByCell(textBox1.Text, 0, 7, 2));*/
         }
 
         /// <summary>
@@ -187,45 +176,21 @@ namespace GenerateProjectFolder
             }
         }
 
-        public static void SetCellValue(ICell cell, object obj)
-        {
-            if (obj.GetType() == typeof(int))
-            {
-                cell.SetCellValue((int)obj);
-            }
-            else if (obj.GetType() == typeof(double))
-            {
-                cell.SetCellValue((double)obj);
-            }
-            else if (obj.GetType() == typeof(IRichTextString))
-            {
-                cell.SetCellValue((IRichTextString)obj);
-            }
-            else if (obj.GetType() == typeof(string))
-            {
-                cell.SetCellValue(obj.ToString());
-            }
-            else if (obj.GetType() == typeof(DateTime))
-            {
-                cell.SetCellValue((DateTime)obj);
-            }
-            else if (obj.GetType() == typeof(bool))
-            {
-                cell.SetCellValue((bool)obj);
-            }
-            else
-            {
-                cell.SetCellValue(obj.ToString());
-            }
-        }
-
+        /// <summary>
+        /// 修改Excel中指定单元格的值
+        /// </summary>
+        /// <param name="filePath">Excel文件路径</param>
+        /// <param name="sheetIndex">sheet页index，0开始</param>
+        /// <param name="row">行，0开始</param>
+        /// <param name="cell">列，0开始</param>
+        /// <param name="cellValue">要修改的单元格数据</param>
+        /// <returns></returns>
         public string WriteToExcelByCell(string filePath, int sheetIndex, int row, int cell, string cellValue)
         {
             IWorkbook wk = null;
             string extension = System.IO.Path.GetExtension(filePath);
             try
             {
-                //FileStream fs = File.OpenRead(filePath);
                 FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
                 if (extension.Equals(".xls"))
                 {
@@ -239,198 +204,28 @@ namespace GenerateProjectFolder
                 }
 
                 fs.Close();
-                //读取当前表数据
-                /*ISheet sheet = wk.GetSheetAt(sheetIndex);
-                ICell icell = sheet.GetRow(row).GetCell(cell);
-                SetCellValue(icell, cellValue);*/
-                //简化上面
+
+                //修改指定单元格数据
                 wk.GetSheetAt(sheetIndex).GetRow(row).GetCell(cell).SetCellValue(cellValue);
-                //MessageBox.Show(sheet.GetRow(row).GetCell(cell).ToString());
 
-                try
-                {
-                    /*FileStream fswrite = File.OpenWrite(filePath);
-                    wk.Write(fswrite);//向打开的这个Excel文件中写入表单并保存。
-                    fswrite.Close();*/
+                MemoryStream ms = new MemoryStream();
+                wk.Write(ms);
+                SaveToFile(ms, textBox1.Text);
 
-
-                    /*
-                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
-                    wk.Write(ms);
-                    ms.Flush();
-                    ms.Position = 0;//流位置归零
-
-                    saveTofle(ms, filePath);*/
-
-                    /*
-                    //Write the stream data of workbook to the root directory
-                    FileStream file = new FileStream(filePath, FileMode.Create);
-                    wk.Write(file);
-                    file.Close();*/
-
-
-
-                    /*try
-                    {
-                        FileStream fsw = File.OpenWrite(filePath);
-                        wk.Write(fsw);//向打开的这个Excel文件中写入表单并保存。  
-                        fsw.Close();
-                        return "true";
-                    }
-                    catch (Exception e)
-                    {
-                        return e.Message;
-                    }*/
-
-
-                    /*byte[] buffer;
-
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        wk.Write(ms);
-                        buffer = ms.ToArray();
-                    }
-
-                    File.WriteAllBytes(filePath, buffer);*/
-
-
-                    /*
-                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
-                    wk.Write(ms);
-                    FileStream files = new FileStream(filePath, FileMode.Create);
-                    //files.Write(ms.ToArray(), 0, ms.ToArray().Length);
-                    byte[] o = ms.ToArray();
-
-                    files.Write(o, 0, o.ToString().Length);
-                    files.Close();*/
-
-
-
-
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        wk.Write(ms);
-                        ms.Flush();
-                        ms.Position = 0;
-                        using (FileStream fss = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite))
-                        {
-                            byte[] d = ms.ToArray();
-                            fss.Write(d, 0, d.Length);
-                            fss.Flush();
-                        }
-                        wk = null;
-                    }
-
-                    /*
-                    FileStream fss = new FileStream(filePath, FileMode.Create);
-                    wk.Write(fss);
-                    fss.Close();
-                    wk = null;*/
-
-
-                    return "true";
-                }
-                catch (Exception e)
-                {
-                    return e.Message;
-                }
-            }
-
-            catch (Exception e)
-            {
-                //只在Debug模式下才输出
-                return e.Message;
-            }
-
-
-
-
-            /*
-            //创建工作薄  
-            IWorkbook wb;
-            string extension = System.IO.Path.GetExtension(filePath);
-            //根据指定的文件格式创建对应的类
-            if (extension.Equals(".xls"))
-            {
-                wb = new HSSFWorkbook();
-            }
-            else
-            {
-                wb = new XSSFWorkbook();
-            }
-
-            //创建一个表单
-            ISheet sheet = wb.CreateSheet("Sheet0");
-            //设置列宽
-            int[] columnWidth = { 10, 10, 20, 10 };
-            for (int i = 0; i < columnWidth.Length; i++)
-            {
-                //设置列宽度，256*字符数，因为单位是1/256个字符
-                sheet.SetColumnWidth(i, 256 * columnWidth[i]);
-            }
-
-            //测试数据
-            int rowCount = 3, columnCount = 4;
-            object[,] data = {
-        {"列0", "列1", "列2", "列3"},
-        {"", 400, 5.2, 6.01},
-        {"", true, "2014-07-02", DateTime.Now}
-        //日期可以直接传字符串，NPOI会自动识别
-        //如果是DateTime类型，则要设置CellStyle.DataFormat，否则会显示为数字
-    };
-
-            IRow row;
-            ICell cell;
-
-            for (int i = 0; i < rowCount; i++)
-            {
-                row = sheet.CreateRow(i);//创建第i行
-                for (int j = 0; j < columnCount; j++)
-                {
-                    cell = row.CreateCell(j);//创建第j列
-                    cell.CellStyle = j % 2 == 0 ? style1 : style2;
-                    //根据数据类型设置不同类型的cell
-                    object obj = data[i, j];
-                    SetCellValue(cell, data[i, j]);
-                    //如果是日期，则设置日期显示的格式
-                    if (obj.GetType() == typeof(DateTime))
-                    {
-                        cell.CellStyle = dateStyle;
-                    }
-                    //如果要根据内容自动调整列宽，需要先setCellValue再调用
-                    //sheet.AutoSizeColumn(j);
-                }
-            }
-
-            //合并单元格，如果要合并的单元格中都有数据，只会保留左上角的
-            //CellRangeAddress(0, 2, 0, 0)，合并0-2行，0-0列的单元格
-            CellRangeAddress region = new CellRangeAddress(0, 2, 0, 0);
-            sheet.AddMergedRegion(region);
-
-            try
-            {
-                FileStream fs = File.OpenWrite(filePath);
-                wb.Write(fs);//向打开的这个Excel文件中写入表单并保存。  
-                fs.Close();
                 return "true";
             }
+
             catch (Exception e)
             {
                 return e.Message;
-            }*/
+            }
         }
 
-        private void saveTofle(MemoryStream file, string fileName)
-        {
-            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
-            {
-                byte[] buffer = file.ToArray();//转化为byte格式存储
-                fs.Write(buffer, 0, buffer.Length);
-                fs.Flush();
-                buffer = null;
-            }//使用using可以最后不用关闭fs 比较方便
-        }
-
+        /// <summary>
+        /// 保存到文件，如果已存在，则覆盖，否则新建
+        /// </summary>
+        /// <param name="ms">MemoryStream流</param>
+        /// <param name="fileName">文件路径</param>
         static void SaveToFile(MemoryStream ms, string fileName)
         {
             using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
@@ -439,112 +234,6 @@ namespace GenerateProjectFolder
                 fs.Write(data, 0, data.Length);
                 fs.Flush();
                 data = null;
-            }
-        }
-
-        public string WriteToExcel(string filePath)
-        {
-            //创建工作薄  
-            IWorkbook wb;
-            string extension = System.IO.Path.GetExtension(filePath);
-            //根据指定的文件格式创建对应的类
-            if (extension.Equals(".xls"))
-            {
-                wb = new HSSFWorkbook();
-            }
-            else
-            {
-                wb = new XSSFWorkbook();
-            }
-
-            ICellStyle style1 = wb.CreateCellStyle();//样式
-            style1.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Left;//文字水平对齐方式
-            style1.VerticalAlignment = NPOI.SS.UserModel.VerticalAlignment.Center;//文字垂直对齐方式
-                                                                                  //设置边框
-            style1.BorderBottom = NPOI.SS.UserModel.BorderStyle.Thin;
-            style1.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
-            style1.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
-            style1.BorderTop = NPOI.SS.UserModel.BorderStyle.Thin;
-            style1.WrapText = true;//自动换行
-
-            ICellStyle style2 = wb.CreateCellStyle();//样式
-            IFont font1 = wb.CreateFont();//字体
-            font1.FontName = "楷体";
-            font1.Color = HSSFColor.Red.Index;//字体颜色
-            font1.Boldweight = (short)FontBoldWeight.Normal;//字体加粗样式
-            style2.SetFont(font1);//样式里的字体设置具体的字体样式
-                                  //设置背景色
-            style2.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Yellow.Index;
-            style2.FillPattern = FillPattern.SolidForeground;
-            style2.FillBackgroundColor = NPOI.HSSF.Util.HSSFColor.Yellow.Index;
-            style2.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Left;//文字水平对齐方式
-            style2.VerticalAlignment = NPOI.SS.UserModel.VerticalAlignment.Center;//文字垂直对齐方式
-
-            ICellStyle dateStyle = wb.CreateCellStyle();//样式
-            dateStyle.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Left;//文字水平对齐方式
-            dateStyle.VerticalAlignment = NPOI.SS.UserModel.VerticalAlignment.Center;//文字垂直对齐方式
-                                                                                     //设置数据显示格式
-            IDataFormat dataFormatCustom = wb.CreateDataFormat();
-            dateStyle.DataFormat = dataFormatCustom.GetFormat("yyyy-MM-dd HH:mm:ss");
-
-            //创建一个表单
-            ISheet sheet = wb.CreateSheet("Sheet0");
-            //设置列宽
-            int[] columnWidth = { 10, 10, 20, 10 };
-            for (int i = 0; i < columnWidth.Length; i++)
-            {
-                //设置列宽度，256*字符数，因为单位是1/256个字符
-                sheet.SetColumnWidth(i, 256 * columnWidth[i]);
-            }
-
-            //测试数据
-            int rowCount = 3, columnCount = 4;
-            object[,] data = {
-        {"列0", "列1", "列2", "列3"},
-        {"", 400, 5.2, 6.01},
-        {"", true, "2014-07-02", DateTime.Now}
-        //日期可以直接传字符串，NPOI会自动识别
-        //如果是DateTime类型，则要设置CellStyle.DataFormat，否则会显示为数字
-    };
-
-            IRow row;
-            ICell cell;
-
-            for (int i = 0; i < rowCount; i++)
-            {
-                row = sheet.CreateRow(i);//创建第i行
-                for (int j = 0; j < columnCount; j++)
-                {
-                    cell = row.CreateCell(j);//创建第j列
-                    cell.CellStyle = j % 2 == 0 ? style1 : style2;
-                    //根据数据类型设置不同类型的cell
-                    object obj = data[i, j];
-                    SetCellValue(cell, data[i, j]);
-                    //如果是日期，则设置日期显示的格式
-                    if (obj.GetType() == typeof(DateTime))
-                    {
-                        cell.CellStyle = dateStyle;
-                    }
-                    //如果要根据内容自动调整列宽，需要先setCellValue再调用
-                    //sheet.AutoSizeColumn(j);
-                }
-            }
-
-            //合并单元格，如果要合并的单元格中都有数据，只会保留左上角的
-            //CellRangeAddress(0, 2, 0, 0)，合并0-2行，0-0列的单元格
-            CellRangeAddress region = new CellRangeAddress(0, 2, 0, 0);
-            sheet.AddMergedRegion(region);
-
-            try
-            {
-                FileStream fs = File.OpenWrite(filePath);
-                wb.Write(fs);//向打开的这个Excel文件中写入表单并保存。  
-                fs.Close();
-                return "true";
-            }
-            catch (Exception e)
-            {
-                return e.Message;
             }
         }
     }
