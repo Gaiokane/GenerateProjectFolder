002e6c06db545b609d05aeac7bfd3c3b65a4e869
Gaiokane <qk632773809@vip.qq.com>
2020-09-12 23:26:39 +0800

    优化：通用方法（配置文件帮助类新增配置文件初始化，是否有缺失默认键；优化各方法）

--- GenerateProjectFolder/ConfigHelper.cs
+++ GenerateProjectFolder/ConfigHelper.cs
@@ -12,90 +12,104 @@ namespace GenerateProjectFolder
         public static string QuickInsert, QuickInsert_IDIncrement, QuickInsert_RandomNum, QuickInsert_NewID, QuickInsert_NewDateTime, QuickInsert_SameNewID, QuickInsert_RandomStr;
         public static string CONFIGPATH = "./GenerateProjectFolder.exe";
 
-        #region 获取配置文件中快捷插入配置
+        #region 配置文件初始化，检查默认键是否有缺失，有则新增
         /// <summary>
-        /// 获取配置文件中快捷插入配置
+        /// 配置文件初始化，检查默认键是否有缺失，有则新增
         /// </summary>
-        public static void getQuickInsertSettingsByappSettings()
+        public static void init()
         {
-            QuickInsert = RWConfig.GetappSettingsValue("QuickInsert", CONFIGPATH);
-            QuickInsert_IDIncrement = RWConfig.GetappSettingsValue("QuickInsert_IDIncrement", CONFIGPATH);
-            QuickInsert_RandomNum = RWConfig.GetappSettingsValue("QuickInsert_RandomNum", CONFIGPATH);
-            QuickInsert_NewID = RWConfig.GetappSettingsValue("QuickInsert_NewID", CONFIGPATH);
-            QuickInsert_NewDateTime = RWConfig.GetappSettingsValue("QuickInsert_NewDateTime", CONFIGPATH);
-            QuickInsert_SameNewID = RWConfig.GetappSettingsValue("QuickInsert_SameNewID", CONFIGPATH);
-            QuickInsert_RandomStr = RWConfig.GetappSettingsValue("QuickInsert_RandomStr", CONFIGPATH);
+            getAllDefaultappSettings();
+            setDefaultSettingsIfIsNullOrEmpty();
         }
         #endregion
 
-        #region 如果配置文件中无快捷插入配置，则新建配置，值默认
+        #region 获取配置文件中所有默认键值
         /// <summary>
-        /// 如果配置文件中无快捷插入配置，则新建配置，值默认
+        /// 获取配置文件中所有默认键值
         /// </summary>
-        public static void setDefaultQuickInsertSettingsIfIsNullOrEmptyByappSettings()
+        public static void getAllDefaultappSettings()
+        {
+            QuickInsert = getappSettings("QuickInsert");
+            QuickInsert_IDIncrement = getappSettings("QuickInsert_IDIncrement");
+            QuickInsert_RandomNum = getappSettings("QuickInsert_RandomNum");
+            QuickInsert_NewID = getappSettings("QuickInsert_NewID");
+            QuickInsert_NewDateTime = getappSettings("QuickInsert_NewDateTime");
+            QuickInsert_SameNewID = getappSettings("QuickInsert_SameNewID");
+            QuickInsert_RandomStr = getappSettings("QuickInsert_RandomStr");
+        }
+        #endregion
+
+        #region 如果配置文件中有缺失默认键，则新增
+        /// <summary>
+        /// 如果配置文件中有缺失默认键，则新增
+        /// </summary>
+        public static void setDefaultSettingsIfIsNullOrEmpty()
         {
             if (string.IsNullOrEmpty(QuickInsert))
             {
-                RWConfig.SetappSettingsValue("QuickInsert", "QuickInsert_IDIncrement;QuickInsert_RandomNum;QuickInsert_NewID;QuickInsert_NewDateTime;QuickInsert_SameNewID", CONFIGPATH);
+                addappSettings("QuickInsert", "QuickInsert_IDIncrement;QuickInsert_RandomNum;QuickInsert_NewID;QuickInsert_NewDateTime;QuickInsert_SameNewID");
             }
             if (string.IsNullOrEmpty(QuickInsert_IDIncrement))
             {
-                RWConfig.SetappSettingsValue("QuickInsert_IDIncrement", "指定id递增;{{id:x}};x替换为开始的序号，从x开始生成（包含x）", CONFIGPATH);
+                addappSettings("QuickInsert_IDIncrement", "指定id递增;{{id:x}};x替换为开始的序号，从x开始生成（包含x）");
             }
             if (string.IsNullOrEmpty(QuickInsert_RandomNum))
             {
-                RWConfig.SetappSettingsValue("QuickInsert_RandomNum", "指定范围随机数;{{[1-2]}};-左边开始&#xA;-右边结束&#xA;不带小数点生成的随机数也不带", CONFIGPATH);
+                addappSettings("QuickInsert_RandomNum", "指定范围随机数;{{[1-2]}};-左边开始&#xA;-右边结束&#xA;不带小数点生成的随机数也不带");
             }
             if (string.IsNullOrEmpty(QuickInsert_NewID))
             {
-                RWConfig.SetappSettingsValue("QuickInsert_NewID", "生成newid/uuid;{{newid}};有匹配项就替换为新uuid", CONFIGPATH);
+                addappSettings("QuickInsert_NewID", "生成newid/uuid;{{newid}};有匹配项就替换为新uuid");
             }
             if (string.IsNullOrEmpty(QuickInsert_NewDateTime))
             {
-                RWConfig.SetappSettingsValue("QuickInsert_NewDateTime", "指定时间递增;{{timed+x:yyyy-MM-dd HH:mm:ss}};+左边为时间类型（d|h|m|s 对应 日|时|分|秒）&#xA;+右边为递增/递减值", CONFIGPATH);
+                addappSettings("QuickInsert_NewDateTime", "指定时间递增;{{timed+x:yyyy-MM-dd HH:mm:ss}};+左边为时间类型（d|h|m|s 对应 日|时|分|秒）&#xA;+右边为递增/递减值");
             }
             if (string.IsNullOrEmpty(QuickInsert_SameNewID))
             {
-                RWConfig.SetappSettingsValue("QuickInsert_SameNewID", "生成相同newid/uuid;{{samenewid}};一次执行多条sql，使用相同uuid", CONFIGPATH);
+                addappSettings("QuickInsert_SameNewID", "生成相同newid/uuid;{{samenewid}};一次执行多条sql，使用相同uuid");
             }
             if (string.IsNullOrEmpty(QuickInsert_RandomStr))
             {
-                RWConfig.SetappSettingsValue("QuickInsert_RandomStr", "在指定元素中随机选择一项;{{[x；y；z...]}};x、y、z为指定元素，\n能在元素中随机选择一项，\n请将；换成英文分号", CONFIGPATH);
+                addappSettings("QuickInsert_RandomStr", "在指定元素中随机选择一项;{{[x；y；z...]}};x、y、z为指定元素，\n能在元素中随机选择一项，\n请将；换成英文分号");
             }
 
-            //快捷插入中缺失默认配置会自动新增
-            getQuickInsertSettingsByappSettings();
-            string append = "";
-            string[] arrayQuickInsert = QuickInsert.Split(';');
-            if (Array.IndexOf(arrayQuickInsert, "QuickInsert_IDIncrement") == -1)
-            {
-                append += ";QuickInsert_IDIncrement";
-            }
-            if (Array.IndexOf(arrayQuickInsert, "QuickInsert_RandomNum") == -1)
-            {
-                append += ";QuickInsert_RandomNum";
-            }
-            if (Array.IndexOf(arrayQuickInsert, "QuickInsert_NewID") == -1)
-            {
-                append += ";QuickInsert_NewID";
-            }
-            if (Array.IndexOf(arrayQuickInsert, "QuickInsert_NewDateTime") == -1)
-            {
-                append += ";QuickInsert_NewDateTime";
-            }
-            if (Array.IndexOf(arrayQuickInsert, "QuickInsert_SameNewID") == -1)
-            {
-                append += ";QuickInsert_SameNewID";
-            }
-            if (Array.IndexOf(arrayQuickInsert, "QuickInsert_RandomStr") == -1)
-            {
-                append += ";QuickInsert_RandomStr";
-            }
-            //不为空才更新，否则每次运行都会更新
-            if (!string.IsNullOrEmpty(append))
-            {
-                RWConfig.SetappSettingsValue("QuickInsert", QuickInsert + append, CONFIGPATH);
-            }
+            #region (已注释)快捷插入中缺失默认配置会自动新增
+            /*
+                getAllDefaultSettings();
+                string append = "";
+                string[] arrayQuickInsert = QuickInsert.Split(';');
+                if (Array.IndexOf(arrayQuickInsert, "QuickInsert_IDIncrement") == -1)
+                {
+                    append += ";QuickInsert_IDIncrement";
+                }
+                if (Array.IndexOf(arrayQuickInsert, "QuickInsert_RandomNum") == -1)
+                {
+                    append += ";QuickInsert_RandomNum";
+                }
+                if (Array.IndexOf(arrayQuickInsert, "QuickInsert_NewID") == -1)
+                {
+                    append += ";QuickInsert_NewID";
+                }
+                if (Array.IndexOf(arrayQuickInsert, "QuickInsert_NewDateTime") == -1)
+                {
+                    append += ";QuickInsert_NewDateTime";
+                }
+                if (Array.IndexOf(arrayQuickInsert, "QuickInsert_SameNewID") == -1)
+                {
+                    append += ";QuickInsert_SameNewID";
+                }
+                if (Array.IndexOf(arrayQuickInsert, "QuickInsert_RandomStr") == -1)
+                {
+                    append += ";QuickInsert_RandomStr";
+                }
+                //不为空才更新，否则每次运行都会更新
+                if (!string.IsNullOrEmpty(append))
+                {
+                    SetappSettingsValue("QuickInsert", QuickInsert + append);
+                }
+                */
+            #endregion
         }
         #endregion
 
@@ -106,7 +120,7 @@ namespace GenerateProjectFolder
         /// <param name="Key">appSettings键</param>
         /// <param name="Value">appSettings值</param>
         /// <returns>true, false</returns>
-        public static bool SetappSettingsValue(string Key, string Value)
+        public static bool addappSettings(string Key, string Value)
         {
             try
             {
@@ -126,7 +140,7 @@ namespace GenerateProjectFolder
         /// </summary>
         /// <param name="Key">appSettings键</param>
         /// <returns>true, false</returns>
-        public static bool DelappSettingsValue(string Key)
+        public static bool delappSettings(string Key)
         {
             try
             {
@@ -154,7 +168,7 @@ namespace GenerateProjectFolder
         /// <param name="Key">appSettings键</param>
         /// <param name="Value">appSettings值</param>
         /// <returns>true, false</returns>
-        public static bool EditappSettingsValue(string Key, string Value)
+        public static bool editappSettings(string Key, string Value)
         {
             try
             {
@@ -181,7 +195,7 @@ namespace GenerateProjectFolder
         /// </summary>
         /// <param name="Key">appSettings键</param>
         /// <returns>appSettings值</returns>
-        public static string getConfigValueByKey(string Key)
+        public static string getappSettings(string Key)
         {
             string result = RWConfig.GetappSettingsValue(Key, CONFIGPATH);
             return result;
